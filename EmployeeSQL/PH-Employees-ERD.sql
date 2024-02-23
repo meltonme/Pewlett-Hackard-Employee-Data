05dfd874713aeb13338604a7ddfec281a882c8d8
meltonme <mahanesme@gmail.com>
2024-02-23 11:02:44 -0500

    added analysis and other requirements

--- EmployeeSQL/PH-Employees-ERD.sql
+++ EmployeeSQL/PH-Employees-ERD.sql
@@ -1,19 +1,19 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/5gecpR
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/yGoesL
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
 CREATE TABLE "departments" (
-    "dept_no" VARCHAR   NOT NULL,
-    "dept_name" VARCHAR   NOT NULL,
+    "dept_no" VARCHAR(4)   NOT NULL,
+    "dept_name" VARCHAR(20)   NOT NULL,
     CONSTRAINT "pk_departments" PRIMARY KEY (
         "dept_no"
      )
 );
 
 CREATE TABLE "titles" (
-    "title_id" VARCHAR   NOT NULL,
-    "title" VARCHAR   NOT NULL,
+    "title_id" VARCHAR(5)   NOT NULL,
+    "title" VARCHAR(20)   NOT NULL,
     CONSTRAINT "pk_titles" PRIMARY KEY (
         "title_id"
      )
@@ -21,11 +21,11 @@ CREATE TABLE "titles" (
 
 CREATE TABLE "employees" (
     "emp_no" INT   NOT NULL,
-    "emp_title_id" VARCHAR   NOT NULL,
+    "emp_title_id" VARCHAR(5)   NOT NULL,
     "birth_date" DATE   NOT NULL,
-    "first_name" VARCHAR   NOT NULL,
-    "last_name" VARCHAR   NOT NULL,
-    "sex" VARCHAR   NOT NULL,
+    "first_name" VARCHAR(20)   NOT NULL,
+    "last_name" VARCHAR(20)   NOT NULL,
+    "sex" VARCHAR(1)   NOT NULL,
     "hire_date" DATE   NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
@@ -34,14 +34,14 @@ CREATE TABLE "employees" (
 
 CREATE TABLE "dept_emp" (
     "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR   NOT NULL,
+    "dept_no" VARCHAR(4)   NOT NULL,
     CONSTRAINT "pk_dept_emp" PRIMARY KEY (
         "emp_no","dept_no"
      )
 );
 
 CREATE TABLE "dept_manager" (
-    "dept_no" VARCHAR   NOT NULL,
+    "dept_no" VARCHAR(4)   NOT NULL,
     "emp_no" INT   NOT NULL,
     CONSTRAINT "pk_dept_manager" PRIMARY KEY (
         "dept_no","emp_no"
@@ -50,7 +50,7 @@ CREATE TABLE "dept_manager" (
 
 CREATE TABLE "salaries" (
     "emp_no" INT   NOT NULL,
-    "salary" REAL   NOT NULL
+    "salary" INT   NOT NULL
 );
 
 ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
--- EmployeeSQL/PH-TableCode.sql
+++ EmployeeSQL/PH-TableCode.sql
@@ -1,22 +1,22 @@
 CREATE TABLE departments (
-    dept_no VARCHAR NOT NULL,
-    dept_name VARCHAR NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
+    dept_name VARCHAR(20) NOT NULL,
     PRIMARY KEY (dept_no)
 ); 
 
 CREATE TABLE titles (
-    title_id  VARCHAR NOT NULL, 
-    title VARCHAR NOT NULL,
+    title_id  VARCHAR(5) NOT NULL, 
+    title VARCHAR(20) NOT NULL,
     PRIMARY KEY (title_id)
 );
 
 CREATE TABLE employees (
     emp_no INT NOT NULL, 
-    emp_title_id VARCHAR NOT NULL,
+    emp_title_id VARCHAR(5) NOT NULL,
     birth_date DATE NOT NULL, 
-    first_name VARCHAR NOT NULL, 
-    last_name VARCHAR NOT NULL, 
-    sex VARCHAR NOT NULL, 
+    first_name VARCHAR(20) NOT NULL, 
+    last_name VARCHAR(20) NOT NULL, 
+    sex VARCHAR(1) NOT NULL, 
     hire_date DATE NOT NULL,
     PRIMARY KEY (emp_no),
     FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
@@ -24,14 +24,14 @@ CREATE TABLE employees (
 
 CREATE TABLE dept_emp (
     emp_no INT NOT NULL, 
-    dept_no VARCHAR NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     PRIMARY KEY (dept_no, emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 CREATE TABLE dept_manager (
-    dept_no VARCHAR NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     PRIMARY KEY (dept_no, emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
@@ -40,6 +40,6 @@ CREATE TABLE dept_manager (
 
 CREATE TABLE salaries(
     emp_no INT NOT NULL,
-    salary REAL NOT NULL,
+    salary INT,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
